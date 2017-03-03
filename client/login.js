86cbda13bda72064f2cd2f40b714eab93efc4254
rklouda <rgklouda@comcast.net>
2017-03-03 06:29:55 +0000

    test

--- client/login.js
+++ client/login.js
@@ -32,7 +32,7 @@ QB.init(sessionToken, QBApp.appId);
 		console.log('Session created with id ' + result.id);
 		// Get all posts
 		getAllPosts();
-		window.location.href = 'start.html';
+	    location.href = 'start.html';
 	}
     });
   });
